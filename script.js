1df4a575d2ecb5c2ac0b68bfd23f3378aecbf11b
Farman71 <farmanhussain71@gmail.com>
2025-02-01 16:46:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,41 +1,26 @@
-let resolveData = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve([1, 2, 3, 4]);
-    }, 0);  // Resolve immediately
-});
-
-let evenData = (data) => {
-    return new Promise((resolve, reject) => {
+function manipulateData() {
+    return new Promise((resolve) => {
         setTimeout(() => {
-            let evenNum = data.filter(e => e % 2 === 0);
-            document.getElementById("output").innerText = evenNum.join(", "); // Display even numbers
-            resolve(evenNum);
-        }, 1000);  // After 1 second
+            resolve([1, 2, 3, 4]);
+        }, 3000);
+    })
+    .then((data) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(data.filter((num) => num % 2 === 0));
+            }, 1000);
+        });
+    })
+    .then((data) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(data.map((num) => num * 2));
+            }, 2000);
+        });
     });
-};
+}
 
-let multiplyData = (data) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            let multiplyNum = data.map(e => e * 2);
-            document.getElementById("output").innerText = multiplyNum.join(", "); // Display doubled values
-            resolve(multiplyNum);
-        }, 1000);  // After 1 more second
-    });
-};
-
-resolveData
+manipulateData()
     .then((data) => {
-        console.log(data);  // Log initial data
-        return evenData(data);  // Filter even numbers
-    })
-    .then((data) => {
-        console.log(data);  // Log even numbers
-        return multiplyData(data);  // Multiply numbers by 2
-    })
-    .then((data) => {
-        console.log(data);  // Log final transformed data
-    })
-    .catch((error) => {
-        console.error(error);
+        document.getElementById('output').innerText = data.join(",");
     });
